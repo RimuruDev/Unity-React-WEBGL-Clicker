eb8e35ec4902480255e4475dbbeee4d4346b76c8
Rimuru <Rimuru.Dev@gmail.com>
2024-06-29 00:09:05 +0400

    Update UnityApp.js

--- react-project/src/UnityApp.js
+++ react-project/src/UnityApp.js
@@ -3,13 +3,12 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 
 function UnityApp() {
     const { unityProvider, sendMessage, addEventListener, removeEventListener } = useUnityContext({
-        loaderUrl: "UnityBuild/Build/UnityBuild.loader.js",
-        dataUrl: "UnityBuild/Build/UnityBuild.data.unityweb",
-        frameworkUrl: "UnityBuild/Build/UnityBuild.framework.js.unityweb",
-        codeUrl: "UnityBuild/Build/UnityBuild.wasm.unityweb",
+        loaderUrl: "UnityBuild/Build/Clicker.loader.js",
+        dataUrl: "UnityBuild/Build/Clicker.data.unityweb",
+        frameworkUrl: "UnityBuild/Build/Clicker.framework.js.unityweb",
+        codeUrl: "UnityBuild/Build/Clicker.wasm.unityweb",
     });
 
-    // Пример передачи данных в Unity
     useEffect(() => {
         const user = {
             user_id: "666",
@@ -17,10 +16,12 @@ function UnityApp() {
         };
 
         // Путь до папки со скриптом в юнити - Assets/Codebase/UserDataHandler.cs
+        // Главное не забыть вырубить strippint level в minimal так как юнька может вырезать метод ReceiveUserData и тогда пиши пропало)
         sendMessage("UserDataHandler", "ReceiveUserData", JSON.stringify(user));
     }, [sendMessage]);
 
     // Добавление обработчика события для получения данных из Unity
+    // В консольке браузера можно глянуть
     useEffect(() => {
         window.UnityToReact = (message) => {
             console.log("Received from Unity: ", message);
